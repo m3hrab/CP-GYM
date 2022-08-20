0c214d61b04fcce4bc50f0fc2c0b99bb64bcd57b
Mehrab Hossain <mehrab.cse16@gmail.com>
2022-08-20 23:01:22 +0600

    magnets debug

--- magnets.py
+++ magnets.py
@@ -1,13 +1,22 @@
-magnets = []
+count = 0
+temp = 0
 for i in range(int(input())):
-    magnet = input()
+    magnet = int(input())
     # initialization 
-    if len(magnets) == 0:
-        magnets.append(magnet)
-        continue
-    if magnets[-1][-1] != magnet[0]:
-        magnets[-1] = magnets[-1] + magnet
-    else:
-        magnets.append(magnet)
-
-print(len(magnets))
+    # if len(magnets) == 0:
+    #     magnets.append(magnet)
+    #     continue
+    # if magnets[-1][-1] != magnet[0]:
+    #     magnets[-1] = magnets[-1] + magnet
+    # else:
+    #     magnets.append(magnet)
+    if temp == 0:
+        temp = magnet
+        count += 1
+    if magnet != temp:
+        count += 1
+        temp = magnet
+    
+        
+# print(len(magnets))
+print(count)
