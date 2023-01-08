846a35ad4a5212289a50a309713dcd8c0f555fdc
Mehrab Hossain <mehrab.cse16@gmail.com>
2023-01-08 20:21:11 +0600

    hello 2023

---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d",&a);
+    printf("%d", a);
+    printf("Yup, it's the output\n");
+    return 0;
+}
---
+++ test1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Over and out" << endl;
+    return 0;
+}
