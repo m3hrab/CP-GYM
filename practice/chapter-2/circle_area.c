7d0802486b5523c8661496eee07e03da6cee1352
m3hrab <mehrab.cse16@gmail.com>
2023-03-13 01:02:53 +0600

    exploring math.h

---
+++ practice/chapter-2/circle_area.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double r, area, pi;
+    scanf("%lf", &r);
+
+    pi = acos(-1);
+    // area = pi * pow(r,2);
+    printf("%lf",pi);
+    return 0;
+}
