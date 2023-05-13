bf4a8b179021044ed2bc83bfb4c1e2b8496fafe4
m3hrab <mehrab.cse16@gmail.com>
2023-05-14 02:31:11 +0600

    codeforces practice

--- codeforces/A. Cards.py
+++ codeforces/A. Cards.py
@@ -1,2 +1,18 @@
 n = int(input())
 cards = list(map(int, input().split()))
+indexes = [i for i in range(1,n+1)]
+
+# Sorting the cards array
+for i in range(n):
+    for j in range(i, n):
+        if cards[i] > cards[j]:
+            temp = cards[i] 
+            cards[i] = cards[j]
+            cards[j] = temp 
+
+            temp2 = indexes[i]
+            indexes[i] = indexes[j]
+            indexes[j] = temp2
+
+for i in range(n//2):
+    print("%d %d"%(indexes[i], indexes[-(i+1)]))
