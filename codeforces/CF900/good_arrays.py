e183e92ff8e9092ef76d895a7be704039a32f770
m3hrab <mehrab.cse16@gmail.com>
2023-08-24 07:49:39 +0600

    unsolved-good-array

--- codeforces/CF900/good_arrays.py
+++ codeforces/CF900/good_arrays.py
@@ -1,59 +1,16 @@
-import sys
-import math
-
-# Inputs
-def read_int():
-    return int(input())
-
-def read_int_list():
-    return list(map(int, input().split()))
-
-# outputs
-def print_int_list(arr):
-    sys.stdout.write(' '.join(map(str, arr)) + '\n')
-    sys.stdout.flush()
-
-
-def is_sorted(arr):
-    left, right = 0, len(arr) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if arr[mid] > arr[mid + 1]:
-            return False
-
-        if arr[mid] < arr[mid - 1]:
-            return False
-
-        left += 1
-        right -= 1
-
-    return True
-
 def solve():
     for i in range(int(input())):
-        n = read_int()
-        arr = read_int_list()
+        n = int(input())
+        arr =  list(map(int, input().split()))
 
-        count = 0
-        i = 0
+        s = sum(arr)
 
-        if is_sorted(arr):
-            print("0")
-            continue
+        # total sum of b not equal to sum of s
+        #  is it possible to make sum using n items and also a[i]!=b[i]
         
-        while True:
-            arr[i] = max(0, arr[i]-1)
-            if is_sorted(arr):
-                print(count)
-                break 
-            count +=1
-            if i == n-1:
-                i = 0 
-            else:
-                i += 1
-
-
+        # if n = 1 ans = 1
+        # if len(arr) == let(set(arr))
+        
+        
 if __name__ == "__main__":
     solve()
