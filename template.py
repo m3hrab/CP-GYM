ce4f5b5429cd07bdd5459905faf5f7444c1b6a50
m3hrab <mehrab.cse16@gmail.com>
2023-08-29 01:14:06 +0600

    update_template

--- template.py
+++ template.py
@@ -1,9 +1,4 @@
 import sys
-import math
-from collections import defaultdict, Counter, deque
-from itertools import permutations, combinations, product
-from bisect import bisect_left, bisect_right
-from heapq import heappush, heappop, heapify
 
 # Define constants
 INF = float('inf')
@@ -29,42 +24,6 @@ def print_yes_no(condition):
 def print_array(arr, sep=' '):
     print(sep.join(map(str, arr)))
 
-# Define helper functions
-def gcd(a, b):
-    return a if b == 0 else gcd(b, a % b)
-
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def sieve(n):
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-    return primes
-
-def factorize(n):
-    factors = []
-    while n % 2 == 0:
-        factors.append(2)
-        n //= 2
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-    if n > 2:
-        factors.append(n)
-    return factors
 
 def solve():
     pass
