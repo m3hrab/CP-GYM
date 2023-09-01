a6025ffe192173d4ae5849c28fe3b37f9eda7429
m3hrab <mehrab.cse16@gmail.com>
2023-09-02 01:09:37 +0600

    update template

--- template.py
+++ template.py
@@ -1,23 +1,15 @@
 import sys
 
-# Define constants
 INF = float('inf')
-MOD = 10**9 + 7
 
-# Define input functions
+# input functions
 def read_int():
     return int(sys.stdin.readline())
 
 def read_ints():
     return list(map(int, sys.stdin.readline().split()))
 
-def read_str():
-    return sys.stdin.readline().strip()
-
-def read_strs():
-    return list(sys.stdin.readline().split())
-
-# Define output functions
+# output functions
 def print_yes_no(condition):
     print('YES' if condition else 'NO')
 
@@ -26,7 +18,7 @@ def print_array(arr, sep=' '):
 
 
 def solve():
-    pass
+    n = read_int()
 
 if __name__ == '__main__':
     solve()
