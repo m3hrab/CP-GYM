1106facb1df200fb3eff167d37285b81b8d33944
m3hrab <mehrab.cse16@gmail.com>
2023-09-07 22:27:46 +0600

    solved

--- div-3(895)/b.py
+++ div-3(895)/b.py
@@ -18,8 +18,23 @@ def print_array(arr, sep=' '):
 
 
 def solve():
-    n = read_int()
-    d, s = map(int, input().split())
+    for i in range(read_int()):
+        trap_pos = []
+        trap_timer = []
+        k = INF
+        n = read_int()
+        for j in range(n):
+            d, s = map(int, input().split())
+            trap_pos.append(d)
+            trap_timer.append(s)
 
+        for i in range(n):
+            max_dist = (trap_timer[i]-1)//2
+            new_k = trap_pos[i] + max_dist 
+            if new_k < k:
+                k = new_k
+
+        print(k)
+    
 if __name__ == '__main__':
     solve()
