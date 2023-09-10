8e7774a53a90f4d57733dfdd688d895a52ff0a97
m3hrab <mehrab.cse16@gmail.com>
2023-09-10 12:36:09 +0600

    solved: different approach

--- codeforces/CF800/buy_a_shovel.py
+++ codeforces/CF800/buy_a_shovel.py
@@ -2,31 +2,30 @@ import sys
 
 INF = float('inf')
 
-# input functions
 def read_int():
     return int(sys.stdin.readline())
 
 def read_ints():
     return list(map(int, sys.stdin.readline().split()))
 
-# output functions
 def print_yes_no(condition):
     print('YES' if condition else 'NO')
 
-def print_array(arr, sep=' '):
-    print(sep.join(map(str, arr)))
-
-
 def solve():
     k, r = map(int, input().split())
-    x = 1
-
+    count = 0
+    i = 1
+    temp = k*i
     while True:
-        if (k*x)%10 == 0 or (k*x)%10==r:
-            break
-        x += 1
+        if temp%10==0 or (temp-r)%10==0:
+            if count == 0:
+                count = 1
+            break 
+        temp = k*i 
+        i += 1
+        count += 1
 
 
-    print(x)
+    print(count)
 if __name__ == '__main__':
     solve()
