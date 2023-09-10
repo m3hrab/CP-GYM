9749f21fab7d0c32025c912400675d428942d15e
m3hrab <mehrab.cse16@gmail.com>
2023-09-10 12:52:51 +0600

    solved cf 800

---
+++ codeforces/CF800/RestoringThreeNumbers.py
@@ -0,0 +1,28 @@
+import sys
+
+INF = float('inf')
+
+# input functions
+def read_int():
+    return int(sys.stdin.readline())
+
+def read_ints():
+    return list(map(int, sys.stdin.readline().split()))
+
+# output functions
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+
+def solve():
+    n = read_ints()
+    n.sort()
+    for i in range(3):
+        print("%d"%(n[3]-n[i]),end=" ")
+    print()
+
+if __name__ == '__main__':
+    solve()
