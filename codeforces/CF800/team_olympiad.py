09c8f152b505d60ad11a43faacfadd914157ccb1
m3hrab <mehrab.cse16@gmail.com>
2023-09-11 13:42:55 +0600

    update template

---
+++ codeforces/CF800/team_olympiad.py
@@ -0,0 +1,37 @@
+import sys
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input
+def read_int():
+    return int(sys.stdin.readline())
+
+def read_n_int():
+    return map(int, sys.stdin.readline().split())
+
+def read_ints():
+    return list(map(int, sys.stdin.readline().split()))
+
+
+# Faster Output
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+    
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+
+# Main Function
+def solve():
+    n = read_int()
+    t = read_ints()
+
+    programmer = []
+    mathmaticians = []
+    ps = []
+    for i in 
+
+if __name__ == '__main__':
+    solve()
