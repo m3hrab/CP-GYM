81cb11558bd2098b63b102ec5f9e2bdb79c04dea
m3hrab <mehrab.cse16@gmail.com>
2023-09-12 17:13:21 +0600

    solved

---
+++ codeforces/CF800/short-substring.py
@@ -0,0 +1,52 @@
+"""
+
+/  \     /  |          /  |                         /  |      
+$$  \   /$$ |  ______  $$ |____    ______   ______  $$ |____  
+$$$  \ /$$$ | /      \ $$      \  /      \ /      \ $$      \ 
+$$$$  /$$$$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$  |$$$$$$$  |
+$$ $$ $$/$$ |$$    $$ |$$ |  $$ |$$ |  $$/ /    $$ |$$ |  $$ |
+$$ |$$$/ $$ |$$$$$$$$/ $$ |  $$ |$$ |     /$$$$$$$ |$$ |__$$ |
+$$ | $/  $$ |$$       |$$ |  $$ |$$ |     $$    $$ |$$    $$/ 
+$$/      $$/  $$$$$$$/ $$/   $$/ $$/       $$$$$$$/ $$$$$$$/ 
+                                    
+
+"""
+
+
+import sys
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input
+def read_int():
+    return int(sys.stdin.readline())
+
+def read_n_int():
+    return map(int, sys.stdin.readline().split())
+
+def read_ints():
+    return list(map(int, sys.stdin.readline().split()))
+
+
+# Faster Output
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+    
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+
+# Main Function
+def solve():
+    for i in range(read_int()):
+        a = input()
+        new = ''
+        for i in range(0,len(a),2):
+            new += a[i]
+        new += a[-1]
+        print(new)
+
+if __name__ == '__main__':
+    solve()
