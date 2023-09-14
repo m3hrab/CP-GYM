d1e5c41c6efc229926923eda3ac4a9b84dea9475
m3hrab <mehrab.cse16@gmail.com>
2023-09-14 23:22:50 +0600

    solved

--- unsolveds/even_array.py
+++ unsolveds/even_array.py
@@ -10,3 +10,14 @@ def solve():
 
 if __name__ == "__main__":
     solve()
+
+#1426A
+#1371A
+for i in range(int(input())):
+    n, x = map(int, input().split())
+    
+    if n <= 2:
+        print(1)
+    else:
+        result = (n - 3) // x + 2
+        print(result)
