f5e05e2f5a0c10af972ff929cfc8fe97f24c4190
m3hrab <mehrab.cse16@gmail.com>
2023-09-17 02:41:11 +0600

    solved cf 800

---
+++ codeforces/CF800/black_square.py
@@ -0,0 +1,52 @@
+"""
+/  \     /  |          /  |                         /  |      
+$$  \   /$$ |  ______  $$ |____    ______   ______  $$ |____  
+$$$  \ /$$$ | /      \ $$      \  /      \ /      \ $$      \ 
+$$$$  /$$$$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$  |$$$$$$$  |
+$$ $$ $$/$$ |$$    $$ |$$ |  $$ |$$ |  $$/ /    $$ |$$ |  $$ |
+$$ |$$$/ $$ |$$$$$$$$/ $$ |  $$ |$$ |     /$$$$$$$ |$$ |__$$ |
+$$ | $/  $$ |$$       |$$ |  $$ |$$ |     $$    $$ |$$    $$/ 
+$$/      $$/  $$$$$$$/ $$/   $$/ $$/       $$$$$$$/ $$$$$$$/ 
+"""
+
+
+import sys
+from math import gcd, sqrt
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input
+def read_int():
+    return int(sys.stdin.readline())
+
+def read_n_int():
+    return map(int, sys.stdin.readline().split())
+
+def read_ints():
+    return list(map(int, sys.stdin.readline().split()))
+
+
+# Faster Output
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+    
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+    
+# Main Function
+def solve():
+    a = read_ints()
+    s = list(input())
+
+    calories = 0
+    for i in s:
+        calories += a[int(i)-1]
+
+    print(calories)
+
+
+if __name__ == '__main__':
+    solve()
