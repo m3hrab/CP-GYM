595f4d9231cef789477f85f87d5348a1ee6ddc85
m3hrab <mehrab.cse16@gmail.com>
2023-09-19 01:18:40 +0600

    Template Update 2.0

--- template.py
+++ template.py
@@ -1,44 +1,38 @@
-"""
-/  \     /  |          /  |                         /  |      
-$$  \   /$$ |  ______  $$ |____    ______   ______  $$ |____  
-$$$  \ /$$$ | /      \ $$      \  /      \ /      \ $$      \ 
-$$$$  /$$$$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$  |$$$$$$$  |
-$$ $$ $$/$$ |$$    $$ |$$ |  $$ |$$ |  $$/ /    $$ |$$ |  $$ |
-$$ |$$$/ $$ |$$$$$$$$/ $$ |  $$ |$$ |     /$$$$$$$ |$$ |__$$ |
-$$ | $/  $$ |$$       |$$ |  $$ |$$ |     $$    $$ |$$    $$/ 
-$$/      $$/  $$$$$$$/ $$/   $$/ $$/       $$$$$$$/ $$$$$$$/ 
-"""
-
+# CP Template 0.2
+# Author: Mehrab (@to0th_less)
 
 import sys
 from math import gcd, sqrt
+from itertools import combinations, permutations
 
 # Constants
 INF = float('inf')
 MOD = 1000000007
 
-# Faster Input
+# Faster Input Shortcut
 def read_int():
-    return int(sys.stdin.readline())
-
-def read_n_int():
-    return map(int, sys.stdin.readline().split())
+    return int(sys.stdin.readline().strip())
 
 def read_ints():
+    return map(int, sys.stdin.readline().split())
+
+def read_int_list():
     return list(map(int, sys.stdin.readline().split()))
 
-
-# Faster Output
+# Faster Output Shortcut
 def print_yes_no(condition):
     print('YES' if condition else 'NO')
-    
+
 def print_array(arr, sep=' '):
     print(sep.join(map(str, arr)))
 
-    
 # Main Function
 def solve():
-    # for i in range(read_int())
-    pass
+    # Sample input format for multiple test cases
+    # t = read_int()  
+    # for _ in range(t):
+        # pass 
+    pass 
+
 if __name__ == '__main__':
     solve()
