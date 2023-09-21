1c3466edcef18f1ecafd7730737d7530cc27c329
m3hrab <mehrab.cse16@gmail.com>
2023-09-22 01:24:56 +0600

    solved

---
+++ codeforces/CF800/1D_eraser.py
@@ -0,0 +1,47 @@
+# CP Template 0.2
+# Author: Mehrab (@to0th_less)
+
+import sys
+from math import gcd, sqrt
+from itertools import combinations, permutations
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input Shortcut
+def read_int():
+    return int(sys.stdin.readline().strip())
+
+def read_ints():
+    return map(int, sys.stdin.readline().split())
+
+def read_int_list():
+    return list(map(int, sys.stdin.readline().split()))
+
+# Faster Output Shortcut
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+# Main Function
+def solve():
+    t = read_int()
+    for _ in range(t):
+        n, k = read_ints()
+        paper = input()
+
+        i = ans = 0
+        while i < n:
+            if paper[i] == 'B':
+                    ans += 1
+                    i += k
+            else:
+                i += 1
+
+        print(ans)
+
+if __name__ == '__main__':
+    solve()
