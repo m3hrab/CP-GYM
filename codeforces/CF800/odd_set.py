e6eeb2cef2290e87290f1669d974c7a39556c895
m3hrab <mehrab.cse16@gmail.com>
2023-10-04 23:44:13 +0600

    solved cf 800

---
+++ codeforces/CF800/odd_set.py
@@ -0,0 +1,49 @@
+# CP Template 0.2
+# Author: Mehrab (@to0th_less)
+
+import sys
+from math import gcd, sqrt
+from itertools import combinations, permutations
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input Shortcut
+def read_int():
+    return int(sys.stdin.readline().strip())
+
+def read_ints():
+    return map(int, sys.stdin.readline().split())
+
+def read_int_list():
+    return list(map(int, sys.stdin.readline().split()))
+
+# Faster Output Shortcut
+def print_yes_no(condition):
+    print('Yes' if condition else 'No')
+
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+# Main Function
+def solve():
+    t = read_int()
+    for _ in range(t):
+        n = read_int()
+        a = read_int_list()
+        even = []
+        odd = []
+        for i in a:
+            if i%2==0:
+                even.append(i)
+            else:
+                odd.append(i)
+
+        ans = False
+        if len(even) == len(odd):
+            ans = True 
+        
+        print_yes_no(ans)
+if __name__ == '__main__':
+    solve()
