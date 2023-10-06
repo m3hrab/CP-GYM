9ca2bf0ba10b7db14ccbefd7ba8e5088e451fe7a
m3hrab <mehrab.cse16@gmail.com>
2023-10-07 02:04:56 +0600

    solved

--- codeforces/CF900/parity_sort.py
+++ codeforces/CF900/parity_sort.py
@@ -32,9 +32,49 @@ def solve():
     for _ in range(t):
         n = read_int()
         arr = read_int_list()
-        
+
+        evens = []
+        ev_ind = []
+        odds = []
+        odd_ind = []
+
+        if arr[0] % 2 == 0:
+            is_even = True 
+        else:
+            is_even = False
+
         for i in range(n):
-            if i
+            if arr[i]%2==0:
+                evens.append(arr[i])
+                ev_ind.append(i)
+            else:
+                odds.append(arr[i])
+                odd_ind.append(i)
+
+        evens.sort()
+        ev_ind.sort()
+        odds.sort()
+        odd_ind.sort()
+
+        ans = [0 for i in range(n)]
+
+        for i in range(len(evens)):
+            ans[ev_ind[i]] = evens[i]
+            
+        for i in range(len(odds)):
+            ans[odd_ind[i]] = odds[i]
+
+        flag = True
+        for i in range(n-1):
+            if ans[i] > ans[i+1]:
+                flag = False
+                break
+        
+        print_yes_no(flag)
+
+
+
+            
 
 
 if __name__ == '__main__':
