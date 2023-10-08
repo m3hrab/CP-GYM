cc42fa9361b38a307de062571029e6439d2cc496
m3hrab <mehrab.cse16@gmail.com>
2023-10-08 22:19:16 +0600

    cf 800, two pointers

---
+++ codeforces/CF800/sasha_and_array_coloring.py
@@ -0,0 +1,50 @@
+# CP Template 0.2
+# Author: Mehrab (@to0th_less)
+
+import sys
+from math import gcd, sqrt
+from itertools import combinations, permutations
+
+# Constants
+INF = float('inf')
+MOD = 1000000007
+
+# Faster Input Shortcut
+def read_int():
+    return int(sys.stdin.readline().strip())
+
+def read_ints():
+    return map(int, sys.stdin.readline().split())
+
+def read_int_list():
+    return list(map(int, sys.stdin.readline().split()))
+
+# Faster Output Shortcut
+def print_yes_no(condition):
+    print('YES' if condition else 'NO')
+
+def print_array(arr, sep=' '):
+    print(sep.join(map(str, arr)))
+
+# Main Function
+def solve():
+    t = read_int()
+    for _ in range(t):
+        n = read_int()
+        a = sorted(read_int_list())
+
+        p1 = 0
+        p2 = n - 1
+        cost = 0 
+
+        while p1 <= p2:
+            cost += (a[p2] - a[p1])
+            p1 += 1
+            p2 -= 1
+        
+        print(cost)
+
+
+
+if __name__ == '__main__':
+    solve()
